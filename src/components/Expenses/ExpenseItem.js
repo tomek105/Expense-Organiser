79c4143777ceb7132cda3c6b763a9d3074c42c8b
Tomasz <lisssu14@gmail.com>
2022-01-08 13:23:20 +0100

    Added input form to add new expenses, basic styling for this form, button listeners and React hook useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,15 +2,24 @@ import './ExpenseItem.css';
 import './ExpenseDate';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
+import { useState } from 'react';
 const ExpenseItem = (props) => {
-    
+
+    const [title, setTitle] = useState(props.title) 
+
+    const clickHandler = () =>{
+        setTitle("Updated!")
+        console.log("klik");
+    }
+
     return (
     <Card className='expense-item'>
         <ExpenseDate date={props.date}/>
         <div className='expense-item__description'>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
         </div>
+        <button onClick={clickHandler}></button>
     </Card>  
     );
 }
---
+++ src/components/NewExpenses/NewExpense.js
@@ -0,0 +1,11 @@
+import './NewExpense.css';
+import ExpenseForm from './ExpenseForm';
+function NewExpense() {
+    return (  
+        <div className="new-expense">
+            <ExpenseForm/>
+        </div>
+    );
+}
+
+export default NewExpense;
